7a6ac212d42660a2b54feeb89ed582313ffcd7e3
IvanFenster <ivanfenster@yandex.ru>
2023-12-07 13:30:12 +0300

    Распознование грифа

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+image = cv2.imread("Chord Am.jpeg")
+
+"""
 #создаем детектор
 handsDetector = mp.solutions.hands.Hands()
 # загружаем изображение
-image = cv2.imread("Chord C.jpg")
+
 # Отражаем по горизонтали если надо
 # (Mediapipе распознает именно зеркальное селфи)
 flipped = np.fliplr(image)
@@ -23,4 +26,18 @@ print(results.multi_handedness)
 cv2.imshow("Hands", res_image)
 cv2.waitKey(0)
 # освобождаем ресурсы
-handsDetector.close()
+handsDetector.close()"""
+
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+avg = np.mean(blurred)
+print(avg)
+thresholded = cv2.threshold(blurred, avg, 255, cv2.THRESH_BINARY)[1]
+contours, _ = cv2.findContours(thresholded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+
+
+cv2.imshow('Detected Cards', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
